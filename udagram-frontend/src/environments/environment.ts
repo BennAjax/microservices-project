c3d77f1c0464c1b4e879a6dea79d0b0c88223796
BennAjax <bennkeys1@gmail.com>
2022-09-25 15:36:32 +0100

    update env

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a25aa5c70405c4296aebe3cf77259f41-1948840436.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
